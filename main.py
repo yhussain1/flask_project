a0caa39bf583449023675c337a27c5c1eb027383
Yahya <yahyasajidhussain@gmail.com>
2022-10-03 00:00:24 +0100

    Base started

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/Breakfast")
+def breakfast():
+    return render_template("breakfast.html")
+
+@app.route("/Lunch")
+def lunch():
+    return render_template("lunch.html")
+
+@app.route("/Dinner")
+def breakfast():
+    return render_template("Dinner.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
